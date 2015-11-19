f92b2a856258d422f11c43176d574ad77c69e910
Alex Morega <alex@grep.ro>
2015-11-19 22:51:20 +0200

    skip broken filenames

--- collector/loaders/mof.py
+++ collector/loaders/mof.py
@@ -4,6 +4,12 @@ from ..utils import open_url
 
 class MofLoader(object):
 
+    class ParseError(RuntimeError):
+
+        def __init__(self, filename):
+            msg = "Can't parse filename %r" % filename
+            RuntimeError.__init__(self, msg)
+
     def __init__(self, repo, match='', **config):
         self.repo = repo
         self.match = match
@@ -11,6 +17,9 @@ class MofLoader(object):
     def parse(self, filename):
         m = re.match(r'^mof(?P<part>\d)_(?P<year>\d{4})'
                      r'_(?P<number>\d+)\.pdf$', filename)
+        if m is None:
+            raise self.ParseError(filename)
+
         fields = {k: int(v) for k, v in m.groupdict().items()}
 
         return {
@@ -29,4 +38,7 @@ class MofLoader(object):
                     if self.match and not re.match(self.match, filename):
                         continue
 
-                    yield self.parse(filename)
+                    try:
+                        yield self.parse(filename)
+                    except self.ParseError as e:
+                        print e
