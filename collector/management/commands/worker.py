5cbb06dc0ad1b2456109b709e08d43d54ed69548
Alex Morega <alex@grep.ro>
2015-11-19 21:46:45 +0200

    start several worker threads

--- collector/management/commands/worker.py
+++ collector/management/commands/worker.py
@@ -1,3 +1,4 @@
+import threading
 from django.core.management.base import BaseCommand
 from ... import index
 
@@ -6,5 +7,17 @@ class Command(BaseCommand):
 
     help = "Run an indexing worker"
 
-    def handle(self, verbosity, **options):
-        index.work_loop()
+    def add_arguments(self, parser):
+        parser.add_argument('--threads', default=1, type=int)
+
+    def handle(self, verbosity, threads, **options):
+        thread_list = [
+            threading.Thread(target=index.work_loop)
+            for _ in range(threads)
+        ]
+
+        for thread in thread_list:
+            thread.start()
+
+        for thread in thread_list:
+            thread.join()
