29f5dcd147a30ce86b0af9324ad686f43ebb2d99
Alex Morega <alex@grep.ro>
2015-11-25 10:45:01 +0200

    refactor collection_slugs

--- collector/views.py
+++ collector/views.py
@@ -15,22 +15,23 @@ class JsonResponse(HttpResponse):
         )
 
 
-def collection_slugs(request):
-    rv = (c.slug for c in Collection.objects_for_user(request.user))
-    if 'collections' in request.GET:
-        collections = set(request.GET['collections'].split())
+def collection_slugs(user, collections_arg):
+    rv = (c.slug for c in Collection.objects_for_user(user))
+    if collections_arg is not None:
+        collections = set(collections_arg.split())
         rv = (slug for slug in rv if slug in collections)
     return rv
 
 
 def home(request):
+    collections_arg = request.GET.get('collections')
     return render(request, 'home.html', {
         'collections': Collection.objects_for_user(request.user),
-        'selected': set(collection_slugs(request)),
+        'selected': set(collection_slugs(request.user, collections_arg)),
     })
 
 
 @csrf_exempt
 def search(request):
-    res = es.search(request.POST['q'], list(collection_slugs(request)))
+    res = es.search(request.POST['q'], list(collection_slugs(request.user, None)))
     return JsonResponse(res)
