e76fcc0d3e34770f7e7ef4048501453fbc33c8a8
Alex Morega <alex@grep.ro>
2015-11-25 03:14:03 +0200

    load collector app before admin for its templates

--- hoover/settings/common.py
+++ hoover/settings/common.py
@@ -1,11 +1,11 @@
 INSTALLED_APPS = (
+    'collector',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'collector',
 )
 
 MIDDLEWARE_CLASSES = (
