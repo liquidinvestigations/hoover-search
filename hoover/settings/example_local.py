114bf1d9398107ba55d49b1a6cd351be1c6f3c2c
Schneider Werner-Walter <noreply@schnwalter.eu>
2015-12-19 17:28:34 +0200

    Update example settings file.

--- hoover/settings/example_local.py
+++ hoover/settings/example_local.py
@@ -1,12 +1,22 @@
+"""
+This is an example settings file. Copy it to `local.py`.
+"""
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-SECRET_KEY = 'something random'
+
 DEBUG = True
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'hoover',
-#    },
-#}
+SECRET_KEY = 'something random'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        # Database info.
+        # see: https://docs.djangoproject.com/en/1.9/ref/settings/#std:setting-DATABASES
+        'NAME': 'hoover',
+        'USER': 'postgres',
+    },
+}
+
 STATIC_ROOT = BASE_DIR + '/static'
 HOOVER_UPLOADS_ROOT = BASE_DIR + '/uploads'
