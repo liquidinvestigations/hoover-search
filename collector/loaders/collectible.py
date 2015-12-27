e33ebef780e40ba33d1c21ff624ff969070a9944
Alex Morega <alex@grep.ro>
2015-12-27 21:08:14 +0200

    use safe_load when parsing yaml

--- collector/loaders/collectible.py
+++ collector/loaders/collectible.py
@@ -28,11 +28,11 @@ class Loader(object):
     def documents(self):
         index_url = Url(self.index)
         with index_url.open() as i:
-            index = yaml.load(i)
+            index = yaml.safe_load(i)
             for doc in index['documents']:
                 doc_url = index_url.join(doc)
                 with doc_url.open() as d:
-                    for item in yaml.load_all(d):
+                    for item in yaml.safe_load_all(d):
                         if self.match:
                             if not re.search(self.match, item['slug']):
                                 continue
