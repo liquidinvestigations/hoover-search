912af9441c573ef0a975a87d72c5499008b3bd8d
Alex Morega <alex@grep.ro>
2016-02-15 00:52:36 +0200

    reindex a previously uploaded collection

--- collector/loaders/upload.py
+++ collector/loaders/upload.py
@@ -1,3 +1,43 @@
+from pathlib import Path
+import subprocess
+from django.conf import settings
+
+UPLOADS_ROOT = Path(settings.HOOVER_UPLOADS_ROOT)
+
+
+class Document:
+
+    def __init__(self, local_path):
+        relative_path = str(local_path.relative_to(UPLOADS_ROOT))
+        self.local_path = local_path
+        self.id = relative_path
+        self.url = (
+            settings.HOOVER_BASE_URL +
+            settings.HOOVER_UPLOADS_URL +
+            relative_path
+        )
+
+    @property
+    def metadata(self):
+        return {
+            'id': self.id,
+            'title': self.id,
+            'url': self.url,
+        }
+
+    def text(self):
+        args = ['pdftotext', str(self.local_path), '-']
+        return subprocess.check_output(args).decode('utf-8')
+
+
+def walk(folder):
+    for item in folder.iterdir():
+        if item.is_dir():
+            yield from walk(item)
+            continue
+        yield item
+
+
 class Loader:
 
     label = "Upload"
@@ -7,3 +47,7 @@ class Loader:
 
     def get_metadata(self):
         return self.config
+
+    def documents(self):
+        for item in walk(UPLOADS_ROOT / self.config['name']):
+            yield Document(item)
