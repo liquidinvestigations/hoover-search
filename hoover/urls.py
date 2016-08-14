4f5e92182d44bf39ff0b208ee8c2c64cedc2c3cb
Alex Morega <alex@grep.ro>
2016-08-14 16:13:21 +0300

    register views by reference, not with string

--- hoover/urls.py
+++ hoover/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from collector.admin import admin_site
+from collector import views, uploads
 
 urlpatterns = [
     url(r'^admin/', include(admin_site.urls)),
-    url(r'^$', 'collector.views.home', name='home'),
-    url(r'^search$', 'collector.views.search', name='search'),
-    url(r'^whoami$', 'collector.views.whoami'),
-    url(r'^collections$', 'collector.views.collections', name='collections'),
-    url(r'^doc/(?P<collection_name>[^/]+)/(?P<id>.+)$', 'collector.views.doc'),
+    url(r'^$', views.home, name='home'),
+    url(r'^search$', views.search, name='search'),
+    url(r'^whoami$', views.whoami),
+    url(r'^collections$', views.collections, name='collections'),
+    url(r'^doc/(?P<collection_name>[^/]+)/(?P<id>.+)$', views.doc),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^uploads/(?P<filename>.+)$', 'collector.uploads.serve_file'),
+    url(r'^uploads/(?P<filename>.+)$', uploads.serve_file),
 ]
