671985e8d579054f8c267e4107890fc275e4b8f2
Alex Morega <alex@grep.ro>
2016-08-15 12:40:30 +0300

    remove obsolete setting ELASTICSEARCH_INDEX_PREFIX

--- testsuite/settings.py
+++ testsuite/settings.py
@@ -5,7 +5,6 @@ testsuite_dir = Path(__file__).absolute().parent
 
 del ELASTICSEARCH_URL
 SECRET_KEY = 'testing secret key'
-ELASTICSEARCH_INDEX_PREFIX = 'hoovertest-'
 HOOVER_UPLOADS_ROOT = str(testsuite_dir / 'uploads')
 
 from hoover.site.settings.testing_local import *
