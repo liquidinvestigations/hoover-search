bd040b4df15ceda5543cd3ce19f115078e9e7334
Alex Morega <alex@grep.ro>
2016-10-03 19:39:52 +0300

    fix twofactor tests
    supersedes pull request #5

--- testsuite/test_twofactor.py
+++ testsuite/test_twofactor.py
@@ -10,6 +10,8 @@ from .fixtures import listen
 
 pytestmark = pytest.mark.django_db
 
+INVITATION_DURATION = 30 * 60 # 30 minutes
+
 @pytest.yield_fixture
 def mock_time(monkeypatch):
     class mock_time:
@@ -60,7 +62,7 @@ def test_flow(client, listen,
     t1 = t0 + timedelta(minutes=minutes)
 
     mock_time(t0)
-    url = invitations.invite('john', create=True)
+    url = invitations.invite('john', INVITATION_DURATION, create=True)
     assert not _access_homepage(client)
 
     mock_time(t1)
@@ -108,7 +110,7 @@ def _accept(invitation, password):
 ])
 def test_login(client, listen, username, password, interval, success):
     login_failure = listen(signals.login_failure)
-    invitations.invite('john', create=True)
+    invitations.invite('john', INVITATION_DURATION, create=True)
     device = _accept(models.Invitation.objects.get(), 'pw')
     assert not _access_homepage(client)
     client.post('/accounts/login/', {
@@ -128,7 +130,7 @@ def test_auto_logout(client, mock_time, listen):
     t0 = datetime(2016, 6, 13, 12, 0, 0, tzinfo=utc)
 
     mock_time(t0)
-    invitations.invite('john', create=True)
+    invitations.invite('john', INVITATION_DURATION, create=True)
     device = _accept(models.Invitation.objects.get(), 'pw')
     assert not _access_homepage(client)
     client.post('/accounts/login/', {
