73458b2c9effda8bcb014edf8245bacb3c43c8b5
Alex Morega <alex@grep.ro>
2016-11-04 13:31:42 +0200

    enumerate documents with jsonapi loader

---
+++ hoover/search/loaders/jsonapi.py
@@ -0,0 +1,60 @@
+"""
+Loader for https://github.com/hoover/search/wiki/Collections-API
+"""
+
+from urllib.parse import urljoin
+from functools import lru_cache
+from django.http import HttpResponse, Http404
+import requests
+from .. import ui, models
+
+@lru_cache(maxsize=32)
+def get_json(url):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise RuntimeError("Unexpected response from {}: {!r}"
+            .format(url, resp))
+    return resp.json()
+
+class Api:
+
+    def __init__(self, meta_url):
+        self.meta_url = meta_url
+
+    def meta(self):
+        return get_json(self.meta_url)
+
+    def feed(self):
+        url = urljoin(self.meta_url, self.meta()['feed'])
+        while True:
+            resp = get_json(url)
+            yield from resp['documents']
+
+            next_url = resp.get('next')
+            if not next_url:
+                break
+            url = urljoin(url, next_url)
+
+class Document:
+
+    def __init__(self, loader, id):
+        self.loader = loader
+        self.id = id
+
+    def view(self, request, suffix):
+        raise NotImplementedError
+
+class Loader:
+
+    label = "Json API"
+
+    def __init__(self, collection, **config):
+        self.api = Api(config['url'])
+
+    def documents(self):
+        for item in self.api.feed():
+            print(item)
+        return iter([])
+
+    def get(self, doc_id):
+        return Document(self, doc_id)
