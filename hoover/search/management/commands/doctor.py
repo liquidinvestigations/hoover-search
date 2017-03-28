ed034e20834e3dfe249d39b2a8d59763546b59e9
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-03-28 12:53:14 +0300

    check indexes and urls for external collections

--- hoover/search/management/commands/doctor.py
+++ hoover/search/management/commands/doctor.py
@@ -8,6 +8,8 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.db import connections
 from django.db.utils import OperationalError
+from ...models import Collection
+from ...es import elasticsearch
 
 
 def http_get_content(link):
@@ -29,6 +31,8 @@ class Command(BaseCommand):
             ('events_dir', self.check_events_dir, settings.HOOVER_EVENTS_DIR is None)
         ]
 
+        checkers += self.get_collection_checkers()
+
         have_errors = False
         for name, check_fun, skip in checkers:
             if skip:
@@ -42,6 +46,19 @@ class Command(BaseCommand):
                     have_errors = True
                     self.print_error(name + " failed the check.")
             self.print_message('')
+
+        if not have_errors:
+            self.print_message("Checking all external collections.")
+            for name, check_fun, collection in  self.get_collection_checkers():
+                collection_string = "{}: '{}'".format(collection.id, collection.name)
+                self.print_message("  Checking   " + collection_string)
+                result = check_fun()
+                if result:
+                    self.print_success("  Collection " + collection_string + " is ok.")
+                else:
+                    have_errors = True
+                    self.print_error("  Collection " + collection_string + " has failed a check.")
+                self.print_message('')
 
         if have_errors:
             self.print_error("The setup has failed some checks.")
@@ -99,6 +116,50 @@ class Command(BaseCommand):
         events_dir = settings.HOOVER_EVENTS_DIR
         if not Path(events_dir).exists() or not Path(events_dir).is_dir():
             self.print_error("The path supplied in HOOVER_EVENTS_DIR is not a valid folder.")
+            return False
+        return True
+
+    def get_collection_checkers(self):
+        collections = Collection.objects.all()
+        return [
+            (
+                "collection " + collection.name,
+                lambda: self.check_collection(collection),
+                collection,
+            )
+            for collection in collections
+            if collection.loader == 'hoover.search.loaders.external.Loader']
+
+    def check_collection(self, collection):
+        with elasticsearch() as es:
+            good_index = es.indices.exists(collection.index)
+        try:
+            options = json.loads(collection.options)
+        except json.JSONDecodeError:
+            self.print_error("  The option string for collection {}".format(collection.name))
+            self.print_error("  is not valid JSON.")
+            return False
+
+        if 'url' not in options:
+            self.print_error("  The option string for collection {}".format(collection.name))
+            self.print_error('  does not have the "url" key set.')
+            return False
+        else:
+            good_endpoint = self.check_external_collection_endpoint(options['url'])
+        return good_index and good_endpoint
+
+    def check_external_collection_endpoint(self, url):
+        content = http_get_content(url)
+        if not content:
+            self.print_error("  Could not connect to the collection endpoint at {}".format(url))
+            return False
+        try:
+            data = json.loads(content)
+        except json.JSONDecodeError:
+            self.print_error("  The endpoint at {} did not return valid json.".format(url))
+            return False
+        if 'feed' not in data:
+            self.print_error('  The endpoint at {} did not return a value for the "feed" key'.format(url))
             return False
         return True
 
