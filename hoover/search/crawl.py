221a08209d811af0aeebcb371c9f12df9c1f2d6b
Alex Morega <alex@grep.ro>
2017-06-08 23:25:28 +0300

    reorganize crawl loop

--- hoover/search/crawl.py
+++ hoover/search/crawl.py
@@ -3,36 +3,37 @@ from django.db import transaction
 from . import es
 
 def iter_collection(collection):
-    with transaction.atomic():
-        collection = (
-            type(collection).objects
-            .select_for_update()
-            .get(id=collection.id)
-        )
-        loader = collection.get_loader()
-        state = json.loads(collection.crawl_state)
+    while True:
+        with transaction.atomic():
+            collection = (
+                type(collection).objects
+                .select_for_update()
+                .get(id=collection.id)
+            )
+            loader = collection.get_loader()
+            state = json.loads(collection.crawl_state)
 
-        if not state:
-            state = {'stack': [loader.get_root_id()]}
+            if not state:
+                state = {'stack': [loader.get_root_id()]}
 
-        def save_state():
-            collection.crawl_state = json.dumps(state)
-            collection.save()
-
-        while state['stack']:
             doc_id = state['stack'].pop()
             doc = loader.get(doc_id)
+
             try:
                 data = doc.get_data()
+
             except loader.DigestError:
-                continue
+                pass
+
             else:
                 yield (doc, data)
-            state['stack'].extend(child['id'] for child in data['children'])
-            save_state()
+                state['stack'].extend(child['id'] for child in data['children'])
 
-        state = None
-        save_state()
+            if not state['stack']:
+                state = None
+
+            collection.crawl_state = json.dumps(state)
+            collection.save()
 
 
 def crawl_collection(collection):
