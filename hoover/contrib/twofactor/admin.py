a1daff13dc7fab9d43e738419b03bc0595f6eb0e
Alex Morega <alex@grep.ro>
2017-11-16 18:30:00 +0200

    admin action to generate invitation

---
+++ hoover/contrib/twofactor/admin.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.shortcuts import render
+from .invitations import invite
+
+
+def create_invitations(modeladmin, request, queryset):
+    username_list = [u.username for u in queryset]
+    duration = settings.HOOVER_TWOFACTOR_INVITATION_VALID
+    invitations = [
+        (username, invite(username, duration))
+        for username in username_list
+    ]
+    return render(request, 'admin-create-invitations.html', {
+        'invitations': invitations,
+    })
+
+
+create_invitations.short_description = "Create invitations"
