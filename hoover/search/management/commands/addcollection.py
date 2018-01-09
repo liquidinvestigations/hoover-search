d4a5a05b0a8c88e4d4482450e0cfc9758e0469bd
Alex Morega <alex@grep.ro>
2018-01-09 18:25:50 +0200

    `--public` flag for `addcollection`

--- hoover/search/management/commands/addcollection.py
+++ hoover/search/management/commands/addcollection.py
@@ -10,12 +10,14 @@ class Command(BaseCommand):
         parser.add_argument('name')
         parser.add_argument('url')
         parser.add_argument('--index')
+        parser.add_argument('--public', action='store_true')
 
-    def handle(self, name, url, index, **kwargs):
+    def handle(self, name, url, index, public, **kwargs):
         models.Collection.objects.create(
             title=name.title(),
             name=name,
             index=index or name,
+            public=public,
             loader='hoover.search.loaders.external.Loader',
             options=json.dumps({'url': url}),
         )
