d0e7003c99d6012eb16bd16cd13b362cdafa0978
Mugur Rus <mugur.rus@gmail.com>
2018-12-18 22:03:18 +0200

    update collections doc view url

--- hoover/search/loaders/upload.py
+++ hoover/search/loaders/upload.py
@@ -1,19 +1,24 @@
 from pathlib import Path
 import subprocess
+from urllib.parse import quote
+
 from django.conf import settings
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
+
 from .. import tika
+
 
 UPLOADS_ROOT = Path(settings.HOOVER_UPLOADS_ROOT)
 
 
 class Document:
 
-    def __init__(self, root, id):
-        self.path = root / id
+    def __init__(self, root, _id):
+        self.path = root / _id
         if root not in self.path.parents:
             raise RuntimeError("Relative path goes outside collection root")
-        self.id = str(id)
+        self.id = str(_id)
 
     @property
     def metadata(self):
@@ -41,13 +46,11 @@ class Document:
         if request.GET.get('raw') == 'on':
             with self._open() as tmp:
                 return HttpResponse(tmp.read(), content_type=mime_type)
-
         else:
             if settings.HOOVER_PDFJS_URL and mime_type == 'application/pdf':
                 raw = request.build_absolute_uri() + '?raw=on'
                 url = settings.HOOVER_PDFJS_URL + 'viewer.html?file=' + quote(raw)
                 return HttpResponseRedirect(url)
-
             else:
                 with self._open() as tmp:
                     html = tika.html(tmp)
