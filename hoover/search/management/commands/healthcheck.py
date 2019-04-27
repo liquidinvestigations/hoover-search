7bb41e8ce27135141815a7d066ccbcf55161ffac
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-04-27 03:33:21 +0300

    Swap out status for healthcheck management command

---
+++ hoover/search/management/commands/healthcheck.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.db.migrations.executor import MigrationExecutor
+from django.db import connections, DEFAULT_DB_ALIAS
+
+def is_database_synchronized():
+    connection = connections[DEFAULT_DB_ALIAS]
+    connection.prepare_database()
+    executor = MigrationExecutor(connection)
+    targets = executor.loader.graph.leaf_nodes()
+    return False if executor.migration_plan(targets) else True
+
+
+class Command(BaseCommand):
+    help = "Check service health: migrations, dependencies"
+
+    def handle(self, **options):
+        assert is_database_synchronized(), 'Migrations not run'
+        print('database ok')
