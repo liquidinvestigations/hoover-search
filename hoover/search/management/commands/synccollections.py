2acaa1fe7ba25b29168a284b67e7584ef918ca4a
Alex Morega <alex@grep.ro>
2020-02-20 13:04:44 +0000

    Create missing collections, update existing ones

---
+++ hoover/search/management/commands/synccollections.py
@@ -0,0 +1,31 @@
+import json
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from ... import models
+
+class Command(BaseCommand):
+
+    help = "Register a collection"
+
+    def add_arguments(self, parser):
+        parser.add_argument('snoop_collections')
+
+    def handle(self, snoop_collections, **kwargs):
+        snoop_base_url = settings.SNOOP_BASE_URL
+        assert snoop_base_url
+
+        for conf in json.loads(snoop_collections):
+            name = conf['name']
+            url = f"{snoop_base_url}/collections/{name}/json"
+            col, created = models.Collection.objects.update_or_create(
+                name=name,
+                defaults=dict(
+                    title=name.title(),
+                    index=name,
+                    public=conf.get('public', True),
+                    loader='hoover.search.loaders.external.Loader',
+                    options=json.dumps({'url': url}),
+                ),
+            )
+            action = "Created" if created else "Updated"
+            print(action, col)
