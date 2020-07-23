89fc380701977b0954317ec4c8ee3bab1e70cc88
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-07-23 19:45:57 +0300

    lock table when syncing collections

--- hoover/search/management/commands/synccollections.py
+++ hoover/search/management/commands/synccollections.py
@@ -3,6 +3,22 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from ... import models
 
+from contextlib import contextmanager
+from django.db import transaction
+from django.db.transaction import get_connection
+
+
+# https://stackoverflow.com/a/54403001
+@contextmanager
+def lock_table(model):
+    with transaction.atomic():
+        cursor = get_connection().cursor()
+        cursor.execute(f'LOCK TABLE {model._meta.db_table}')
+        try:
+            yield
+        finally:
+            cursor.close()
+
 
 class Command(BaseCommand):
     help = "Register a collection"
@@ -16,22 +32,25 @@ class Command(BaseCommand):
 
         snoop_collections = json.loads(snoop_collections_json)
         print('json string has', len(snoop_collections), 'collections')
-        to_delete = models.Collection.objects.exclude(name__in=[c['name'] for c in snoop_collections])
-        print('Deleting', to_delete.count(), 'collections')
-        to_delete.delete()
 
-        for conf in snoop_collections:
-            name = conf['name']
-            print('Handling collection', name)
-            col, created = models.Collection.objects.update_or_create(
-                name=name,
-                defaults=dict(
-                    index=name,
-                ),
-            )
-            if created or not col.title:
-                col.title = name
-                col.save()
+        print('locking table...')
+        with lock_table(models.Collection):
+            to_delete = models.Collection.objects.exclude(name__in=[c['name'] for c in snoop_collections])
+            print('Deleting', to_delete.count(), 'collections')
+            to_delete.delete()
 
-            action = "Created" if created else "Updated"
-            print(action, col)
+            for conf in snoop_collections:
+                name = conf['name']
+                print('Handling collection', name)
+                col, created = models.Collection.objects.update_or_create(
+                    name=name,
+                    defaults=dict(
+                        index=name,
+                    ),
+                )
+                if created or not col.title:
+                    col.title = name
+                    col.save()
+
+                action = "Created" if created else "Updated"
+                print(action, col)
