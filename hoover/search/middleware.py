31f54a75876798c40a88300c9ef68822f783f75c
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-10-14 16:20:05 +0300

    remove middleware that set user from header

--- hoover/search/middleware.py
+++ hoover/search/middleware.py
@@ -1,7 +1,5 @@
 from django.utils.cache import add_never_cache_headers
 from django.utils.deprecation import MiddlewareMixin
-from django.conf import settings
-from django.contrib.auth.middleware import RemoteUserMiddleware
 
 
 class NoReferral(MiddlewareMixin):
@@ -17,22 +15,3 @@ class NoCache(MiddlewareMixin):
         if 'Cache-Control' not in response:
             add_never_cache_headers(response)
         return response
-
-
-class AuthproxyUserMiddleware(RemoteUserMiddleware):
-
-    header = 'HTTP_X_FORWARDED_USER'
-    is_admin_header = 'HTTP_X_FORWARDED_USER_ADMIN'
-
-    def process_request(self, request):
-        if not settings.HOOVER_AUTHPROXY:
-            return
-
-        super().process_request(request)
-
-        user = request.user
-        is_admin = (request.META.get(self.is_admin_header) == 'true')
-        if is_admin != user.is_superuser or is_admin != user.is_staff:
-            user.is_superuser = is_admin
-            user.is_staff = is_admin
-            user.save()
--- hoover/site/settings/common.py
+++ hoover/site/settings/common.py
@@ -18,7 +18,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'hoover.search.middleware.AuthproxyUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
