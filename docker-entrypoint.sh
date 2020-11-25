44c9115c1fb82fdc772f63685f734d82ba0108c1
Morten Stehr <morten.stehr@spiegel.de>
2020-11-25 11:49:04 +0100

    chown

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir -p /opt/hoover/metrics
 chown -R 666:666 /opt/hoover/metrics
 
-# chown $UID:$GID $DATA_DIR/*
+chown -R $UID:$GID $DATA_DIR/*
 
 exec gosu $USER_NAME "$@"
 # sudo -Eu $USER_NAME "$@"
