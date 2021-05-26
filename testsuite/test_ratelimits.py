3b1bc180dc0b7a5da7eba0993f1c326eba800054
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-05-26 16:42:01 +0200

    add test

--- testsuite/test_ratelimits.py
+++ testsuite/test_ratelimits.py
@@ -7,6 +7,9 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
-def test_search_rate(api):
+def test_search_rate(client):
+    url = reverse('search')
+    payload = {'query': {'match_all': {}}, 'collections': ['testcol']}
     for _ in range(30):
-        api.search({'match_all': {}}, ['testcol'])['hits']['hits']
+        resp = client.post(url, payload, content_type='application/json')
+        assert resp.status_code == 200
