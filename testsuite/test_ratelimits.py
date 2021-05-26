5e799f4aaa5c1faaabd9b7e4eeb4bd4f7e59e892
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-05-26 16:54:43 +0200

    add logged in user to test

--- testsuite/test_ratelimits.py
+++ testsuite/test_ratelimits.py
@@ -7,8 +7,10 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
-def test_search_rate(client):
+def test_search_rate(client, django_user_model):
+    user = django_user_model.objects.create_user(username='testuser', password='pw')
     url = reverse('search')
+    client.force_login(user)
     payload = {'query': {'match_all': {}}, 'collections': ['testcol']}
     for _ in range(30):
         resp = client.post(url, payload, content_type='application/json')
