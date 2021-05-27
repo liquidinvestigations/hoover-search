ed026a5ce775cbb97d083cfbdddc453f5356c0ba
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-05-27 12:20:29 +0200

    add test for doc route

--- testsuite/test_ratelimits.py
+++ testsuite/test_ratelimits.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.urls import reverse
 import json
-from .test_search import api
+from .test_search import external, JsonResponse
 import pytest
+from time import sleep
+from hoover.search import models
 
 pytestmark = pytest.mark.django_db
 
@@ -17,3 +19,40 @@ def test_search_rate(client, django_user_model):
         assert resp.status_code == 200
     resp_exceeded = client.post(url, payload, content_type='application/json')
     assert resp_exceeded.status_code == 429
+    sleep(60)
+    resp_after_timeout = client.post(url, payload, content_type='application/json')
+    assert resp_after_timeout == 200
+
+
+@pytest.mark.skip(
+    reason='not working yet')
+def test_search_doc(client, django_user_model, external):
+    user = django_user_model.objects.create_user(username='testuser', password='pw')
+    client.force_login(user)
+
+    col = models.Collection.objects.create(
+        name='testcol',
+        index='hoover-testcol',
+        public=True,
+        loader='hoover.search.loaders.external.Loader',
+        options='{"url": "https://example.com/doc/json"}',
+    )
+
+    external['https://example.com/doc/json'] = JsonResponse({
+        'data_urls': '{id}/json',
+    })
+
+    external['https://example.com/doc/mock1/json'] = JsonResponse({
+        'id': 'mock1',
+        'version': '1',
+        'content': {'text': "hello world"},
+    })
+
+    for _ in range(30):
+        resp = client.post('/doc/testcol/mock1/json')
+        assert resp.status_code == 200
+    resp_exceeded = client.post('/doc/testcol/mock1/json')
+    assert resp_exceeded.status_code == 429
+    sleep(60)
+    resp_after_timeout = client.post('/doc/testcol/mock1/json')
+    assert resp_after_timeout == 200
