f40f880a693dc1a9990138e2aeaeb3e286273c07
Kjell Knudsen <kjell.knudsen@yahoo.de>
2022-12-14 18:30:31 +0100

    fix migrations

--- hoover/search/migrations/0012_collection_avg_batch_time.py
+++ hoover/search/migrations/0012_collection_avg_batch_time.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('search', '0019_batchresultcache'),
+        ('search', '0011_batchresultcache'),
     ]
 
     operations = [
