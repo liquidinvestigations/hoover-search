442e63627ad6f78ab228efd52e3e219588c2c912
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-01-27 12:18:45 +0200

    instrument celery

--- hoover/search/celery.py
+++ hoover/search/celery.py
@@ -1,9 +1,17 @@
 # from https://docs.celeryproject.org/en/stable/django/first-steps-with-django.html
 import os
 
+from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from celery import Celery
+from celery.signals import worker_process_init
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoover.site.settings.docker_local')
+
+
+@worker_process_init.connect(weak=False)
+def init_celery_tracing(*args, **kwargs):
+    CeleryInstrumentor().instrument()
+
 
 app = Celery('search')
 app.config_from_object('django.conf:settings', namespace='CELERY')
