101ca40a845a8c59968dc1ae2d692252d2bdabe9
Kjell Knudsen <kjell.knudsen@yahoo.de>
2023-10-25 14:00:01 +0200

    fix handle function in management command

--- hoover/search/management/commands/syncnextcloudcols.py
+++ hoover/search/management/commands/syncnextcloudcols.py
@@ -5,5 +5,5 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Sync nextcloud collections with snoop"
 
-    def handle(self):
+    def handle(self, *args, **options):
         sync_nextcloud_collections()
