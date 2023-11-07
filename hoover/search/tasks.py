be574f24f7bd6631e30b7002a5c8ff666fd16e68
Kjell Knudsen <kjell.knudsen@yahoo.de>
2023-11-07 13:16:24 +0100

    fix bug and cleanup

--- hoover/search/tasks.py
+++ hoover/search/tasks.py
@@ -15,15 +15,12 @@ def sync_nextcloud_directories(max_depth, max_size):
     users = get_user_model().objects.all()
     for user in users:
         if models.WebDAVPassword.objects.filter(user=user).exists():
-            log.info(f'Found WebDAV-Password for user: {user.get_username()}')
             options = {
                 'webdav_hostname': WEBDAV_ROOT + f'/{user.get_username()}',
                 'webdav_login': user.get_username(),
                 'webdav_password': models.WebDAVPassword.objects.get(user=user).password,
             }
-            log.info(f'{options}')
             client = Client(options)
-            log.info('Created webdav client!')
             directories = recurse_nextcloud_directories('/',
                                                         max_depth,
                                                         client,
@@ -52,16 +49,13 @@ def get_name(path):
 def recurse_nextcloud_directories(path, max_depth, client, username, max_size=20, depth=0):
     # first element is the current directory itself
     content = client.list(path, get_info=True)[1:]
-    log.info(f'Found root content: {[x.get("path") for x in content]}')
     directories = [x for x in content if x['isdir']]
     dir_list = []
     dir_list.extend(directories)
 
     if depth == max_depth:
-        print('Reached max_depth!!!')
         return []
     if len(dir_list) > max_size:
-        print('Dir list to long!!!')
         dir_list = dir_list[:max_size]
         return dir_list
 
@@ -82,13 +76,10 @@ def recurse_nextcloud_directories(path, max_depth, client, username, max_size=20
                                                     client,
                                                     username,
                                                     depth=depth + 1)
-        print(f'new content: {[x.get("path") for x in new_content]}')
         if new_content:
             new_directories = [x for x in new_content if x['isdir']]
             if len(new_directories) + len(dir_list) > max_size:
                 remaining = max_size - len(dir_list)
                 new_directories = new_directories[:remaining]
-                dir_list += new_directories
-                print(f'added to dir_list: {new_directories}')
-    print('dir_list:', [x["path"] for x in dir_list])
+            dir_list += new_directories
     return dir_list
